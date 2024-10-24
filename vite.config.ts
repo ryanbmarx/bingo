fcc9ef23ca757974eea75e92225a41908f708fe4
Ryan Marx <ryan.marx@pulsarinformatics.com>
2024-10-23 20:38:51 -0500

    Set base in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		chunkSizeWarningLimit: 1000 // This is huge, but our usecase is very limited so it is ok.
-	}
+export default defineConfig(({ mode }) => {
+	const base = mode === 'development' ? '' : '/bingo/';
+	return {
+		base,
+		plugins: [sveltekit()],
+		build: {
+			chunkSizeWarningLimit: 1000 // This is huge, but our usecase is very limited so it is ok.
+		}
+	};
 });
